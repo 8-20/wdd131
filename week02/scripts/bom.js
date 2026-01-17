3069a844a431d492a8c8e3f488d97e7133db7ee4
8-20 <scherbelpa@gmail.com>
2026-01-16 20:30:39 -0700

    Finalized BoM Program

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,32 @@
 const input = document.querySelector('#favchap');
-const button = documonent.querySelector('button');
+const button = document.querySelector('button');
 const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+input.focus();
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        input.focus();
+        input.value = '';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+        })
+    } else {
+        input.value = '';
+        input.focus();
+    }
+});
+
+
+
+
+
+
+
